78a61775096c76337d05327b8ff9aa1210cb9016
karthi5555 <105117281+karthi5555@users.noreply.github.com>
2023-12-08 19:50:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length - 3, isCapital = false;
+	let output = "#";
+	while(i < shortHex.length){
+		let ascii = shortHex[i].charCodeAt[0];
+		if(acii >= 60 && ascii <= 90){
+			isCapital = true;
+		}
+
+		output += shortHex[i] + shortHex[i];
+	}
+
+	return isCapital ? output.toUppercase() : output;
+
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+
